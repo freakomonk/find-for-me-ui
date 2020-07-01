22bfa57bcbdff70a50beded5b923d89a8ca535db
freakomonk <karthikeya.surabhi@gmail.com>
2020-07-01 19:07:19 +0530

    App layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css'
+import { Switch, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Report from './components/Report'
+import ErrorPage from './components/ErrorPage'
+import { HomeOutlined, FileAddOutlined, UserOutlined } from '@ant-design/icons'
+const { Header, Footer, Content } = Layout
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>
+         <Menu mode="horizontal" theme="dark">
+           <Menu.Item key="home" icon={<HomeOutlined/>}>
+             <Link to='/'>Home</Link>
+           </Menu.Item>
+           <Menu.Item key="report" icon={<FileAddOutlined />}>
+             <Link to='/report'>Report</Link>
+           </Menu.Item>
+           <Menu.Item key="profile" icon={<UserOutlined />}>
+             <Link to='/profile'>Profile</Link>
+           </Menu.Item>
+         </Menu>
+        </Header>
+        <Content>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/report" component={Report} />
+            <Route path="/profile" component={Profile} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </Content>
+        <Footer>
+
+        </Footer>
+      </Layout>
     </div>
   );
 }
